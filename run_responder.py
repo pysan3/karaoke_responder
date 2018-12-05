ab377d6ab6d48ab1a1343a5ee8d6fce3cb8ec117
Takuto <pysan3@gmail.com>
2018-12-06 01:07:03 +0900

    create git

---
+++ run_responder.py
@@ -0,0 +1,31 @@
+import responder
+import time
+
+api = responder.API()
+
+@api.route("/hello/{who}")
+def say_hello(req, resp, *, who):
+    params = req.params.get("id", "")
+    resp.headers["X-Pizza"] = "42"
+    resp.status_code = 200
+    resp.media = {
+      "Hello": who,
+      "param": params
+    }
+
+@api.background.task
+def sleep(s=10):
+    time.sleep(s)
+    print("slept!")
+
+@api.route("/")
+def hello(req, resp):
+    sleep()
+    resp.content = "processing"
+
+@api.route("/db/login")
+async def login(req, resp):
+    datas = await req.media()
+
+if __name__ == '__main__':
+    api.run()
